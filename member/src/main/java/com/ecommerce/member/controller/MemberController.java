a41a81c089444b1a486e5b42f90e044047a0fcf2
Jack <zhaoke199407@gmail.com>
2024-08-24 10:18:46 -0400

    datasource connection is ok

---
+++ member/src/main/java/com/ecommerce/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.ecommerce.member.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/member")
+public class MemberController {
+
+}
---
+++ order/src/main/java/com/ecommerce/order/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.order.controller;
+
+import com.ecommerce.infrastructure.repository.mapper.order.OrdersBizMapper;
+import com.ecommerce.infrastructure.repository.model.order.Orders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Resource
+    private OrdersBizMapper ordersBizMapper;
+
+    @PostMapping("/insert/batch")
+    public String insertBatch(@RequestBody List<Orders> orders) {
+        ordersBizMapper.insertBatch(orders);
+        return "success";
+    }
+}
